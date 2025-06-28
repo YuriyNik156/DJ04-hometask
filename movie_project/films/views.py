3f640605d19f8c80d3c9280beb0c287234eecefe
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-28 17:13:18 +0300

    Update views.py

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render, redirect
+from .forms import MovieForm
+from .models import Movie
 
+def add_movie(request):
+    error = ""
+    if request.method == "POST":
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("movie_list")
+        else:
+            error = "Данные введены некорректно"
+    else:
+        form = MovieForm()
+    return render(request, 'films/add_movie.html', {'form': form, 'error':error})
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'films/movie_list.html', {'movies': movies})
